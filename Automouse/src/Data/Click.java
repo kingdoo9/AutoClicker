438ac42f2d7b5a0944eb1e56d4511084cdbfb11b
KangSD <KangSD@DESKTOP-4LGM20R>
2018-10-15 23:14:29 +0900

    Click Entity Get_Set function

--- Automouse/src/Data/Click.java
+++ Automouse/src/Data/Click.java
@@ -1,15 +1,35 @@
 package Data;
 
-import java.awt.Color;
-
-import Panels.RightSettingPanel;
-
 public class Click {
 	private int mouseX;
 	private int mouseY;
 	private int Count;
 	private int repeat;
-	private RightSettingPanel right = new RightSettingPanel(10, 10, 475, 445, Color.white);
+
+	public void setX(int x) {
+		this.mouseX = x;
+	}
+	public void setY(int y) {
+		this.mouseY = y;
+	}
+	public void setCount(int c) {
+		this.Count = c;
+	}
+	public void setrepeat(int r) {
+		this.repeat = r;
+	}
 	
+	public int getCount() {
+		return Count;
+	}
+	public int getMouseX() {
+		return mouseX;
+	}
+	public int getMouseY() {
+		return mouseY;
+	}
+	public int getRepeat() {
+		return repeat;
+	}
 	
 }
